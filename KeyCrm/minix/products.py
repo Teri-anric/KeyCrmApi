d3e51fd07fcab31148f80790489c8772c92170b0
Teri <75248049+Teri-anric@users.noreply.github.com>
2023-06-04 16:00:29 +0300

    added comments and get buyer api
    Took 10 minutes

--- KeyCrm/minix/products.py
+++ KeyCrm/minix/products.py
@@ -1,26 +1,26 @@
 from typing import List, Optional, Union, Dict, Any
 
-from KeyCrm.types.product import ProductsResponse, ProductCreateRequest, ShortCustomField, Product
-from KeyCrm.types.category import ListCategories, Category
+from KeyCrm.types.product import ListProducts, Product
+from KeyCrm.types.category import Category, ListCategories
 from KeyCrm.utils import parse_filters
 
 
 class ProductMinix:
-    def get_products(self, limit: int = 15, page: int = 1, include: Optional[str] = None,
-                     filters: Dict[str, Any] = None) -> ProductsResponse:
+    def get_products(self, limit: int = 15, page: int = 1, include: List[str] = None,
+                     filters: Dict[str, Any] = None) -> ListProducts:
         url = "/products"
         params = {'limit': limit if limit else 15, 'page': page if page else 1}
         if include is not None:
-            params['include'] = include
+            params['include'] = ",".join(include)
         if filters is not None:
             params.update(parse_filters(filters))
 
-        return ProductsResponse.parse_obj(self._get_request(url, params))
+        return ListProducts.parse_obj(self._get_request(url, params))
 
-    def get_product(self, product_id: Union[int, str], include: Optional[str] = None):
+    def get_product(self, product_id: Union[int, str], include: List[str] = None) -> Product:
         url = f"/products/{product_id}"
-        params = {} if include is None else {'include': include}
-        Product.parse_obj(self._get_request(url, params))
+        params = {} if include is None else {'include': ",".join(include)}
+        return Product.parse_obj(self._get_request(url, params))
 
     def create_product(self, name: str, description: Optional[str] = None, pictures: Optional[List[str]] = None,
                        currency_code: Optional[str] = None, sku: Optional[str] = None, barcode: Optional[str] = None,
@@ -47,7 +47,7 @@ class ProductMinix:
         )
         self._post_request(url, request_data.dict())
 
-    def get_categories(self, limit: int = 15, page: int = 1, filters: Optional[Dict[str, Union[str, list]]] = None):
+    def get_categories(self, limit: int = 15, page: int = 1, filters: Optional[Dict[str, Union[str, list]]] = None) -> ListCategories:
         url = "/products/categories"
         params = {'limit': limit, 'page': page}
         if filters is not None:
@@ -55,7 +55,7 @@ class ProductMinix:
 
         return ListCategories.parse_obj(self._get_request(url, params))
 
-    def create_category(self, name: str, parent_id: Optional[int] = None):
+    def create_category(self, name: str, parent_id: Optional[int] = None) -> Category:
         url = "/products/categories"
         params = {'name': name, parent_id: 'parent_id'}
-        Category.parse_obj(self._post_request(url, params))
+        return Category.parse_obj(self._post_request(url, params))
